fb04ab92fb4439c2d335bf275c080fb644e4331d
Gabriel Gavin <ggavin2@uncc.edu>
2024-06-20 10:15:03 -0400

    create vector 3 scriptable object

---
+++ Assets/Scripts/Vector3VariableObject.cs
@@ -0,0 +1,7 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Vector3VariableObject : ScriptableObject
+{ public Vector3 Value; }
